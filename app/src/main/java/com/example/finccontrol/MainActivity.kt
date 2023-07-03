fee4f069bf27d49aeb9be912fc07b34b62de68f6
Lucas Caetano <lucas_caetano123@hotmail.com>
2023-07-03 15:44:15 -0300

    feat: viewModel and button for add new transaction

--- app/src/main/java/com/example/finccontrol/MainActivity.kt
+++ app/src/main/java/com/example/finccontrol/MainActivity.kt
@@ -14,22 +14,44 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.finccontrol.ui.theme.FincControlTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class MyViewModel : ViewModel () {
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState: StateFlow<UiState> = _uiState
+
+    fun add(transaction : String){
+        val transactions = _uiState.value.transactions.toMutableList()
+        transactions.add(transaction)
+        _uiState.value = UiState(transactions = transactions )
+    }
+    data class UiState(
+        val transactions: List<String> = emptyList()
+    )
+}
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FincControlTheme{
+            FincControlTheme {
                 Column() {
                     Welcome()
                     Transactions()
@@ -40,21 +62,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Transactions(){
-    LazyColumn(
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth(),
-        contentPadding = PaddingValues(8.dp)
-    ){
-        items(transactionsDummy.size){ index ->
-            Transaction(index)
-        }
-    }
+fun Transactions(viewModel: MyViewModel = viewModel()) {
+    val uiState by viewModel.uiState.collectAsState()
+   Column() {
+       LazyColumn(
+           modifier = Modifier
+               .padding(16.dp)
+               .weight(1f)
+               .fillMaxWidth(),
+           contentPadding = PaddingValues(8.dp)
+       ) {
+           items(uiState.transactions.size) { index ->
+               Transaction(uiState.transactions[index])
+           }
+       }
+       Button(onClick = {
+           viewModel.add("New Transaction")
+       }) {
+           Text(text = "Adicionar transação")
+       }
+   }
 }
 
 @Composable
-private fun Transaction(index: Int) {
+private fun Transaction(transaction: String) {
     Card(
         elevation = CardDefaults.cardElevation(8.dp),
         modifier = Modifier
@@ -71,7 +102,7 @@ private fun Transaction(index: Int) {
             )
             Spacer(modifier = Modifier.padding(16.dp))
             Text(
-                text = transactionsDummy[index],
+                text = transaction,
                 modifier = Modifier.padding(20.dp)
             )
         }
@@ -84,7 +115,7 @@ fun Welcome() {
     Row(
         modifier = Modifier.padding(16.dp),
         verticalAlignment = Alignment.CenterVertically
-    ){
+    ) {
         Text(
             text = "Bem vindo cara, \n Lucas Caetano",
             style = MaterialTheme.typography.headlineLarge,
